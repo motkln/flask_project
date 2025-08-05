ed2285eb2b552b99b94248d2c565b4fa4a836472
Матвей Калинин <pilezam@mail.ru>
2025-08-05 19:44:37 +0300

    add html

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,21 @@
 from app import app
-
+from flask import render_template
 
 @app.route('/hello')
 def hello():
     return 'Hello, world!'
 
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 @app.route('/info')
 def show_info():
